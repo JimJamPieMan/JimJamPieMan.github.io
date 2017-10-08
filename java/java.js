ea482dbe9e615bdbb24df4f38b69fe47326809fc
JimJamPieMan <rudlandjames@gmail.com>
2017-10-08 16:18:15 +1030

    ajavascrpta

---
+++ java/java.js
@@ -0,0 +1,23 @@
+var glass;
+var dot;
+var y = 0;
+var canvas;
+
+
+function setup() {
+  canvas = createCanvas(500,500);
+  background(51);
+  angleMode(DEGREES);
+
+}
+
+function draw() {
+  noFill();
+  stroke(255);
+  color(255);
+  glass = arc(250, 350, 200, 200,0,180);
+
+
+
+
+}
