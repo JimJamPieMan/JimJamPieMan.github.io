49636e04d09c46f7e2a2347bfb06b4d518203919
JimJamPieMan <rudlandjames@gmail.com>
2017-10-31 10:53:37 +1030

    THE FINAL

--- java/java.js
+++ java/java.js
@@ -1,69 +1,85 @@
 var database;
 
-function alerts(){
-alert("This messaging service is in no way encrypted. Do not say anything incriminating or harmful to other people. By clicking close you agree to the previous statement.");
-var config = {
-  apiKey: "AIzaSyCWZugJWhKfWxA-mudw4-lQq9Lf2ruBqPo",
-  authDomain: "achat-7607e.firebaseapp.com",
-  databaseURL: "https://achat-7607e.firebaseio.com",
-  projectId: "achat-7607e",
-  storageBucket: "achat-7607e.appspot.com",
-  messagingSenderId: "454081060002"
+
+function setup(){
+  alert("This messaging service is in no way encrypted. Do not say anything incriminating or harmful to other people. By clicking close you agree to the previous statement.");
+  var config = {
+    apiKey: "AIzaSyCWZugJWhKfWxA-mudw4-lQq9Lf2ruBqPo",
+    authDomain: "achat-7607e.firebaseapp.com",
+    databaseURL: "https://achat-7607e.firebaseio.com",
+    projectId: "achat-7607e",
+    storageBucket: "achat-7607e.appspot.com",
+    messagingSenderId: "454081060002"
   };
   firebase.initializeApp(config);
   database = firebase.database();
-
-  var ref = database.ref('nammess');
- ref.on('value', gotData, errData);
+  loadDatabase();
 }
 
-function enterName(){
-var nameBox = document.getElementById("namebox");
-var errName = document.getElementById("errNa");
-var nameInput = document.getElementById("nameInput");
-if (nameBox.value=="")
-{
-  nameBox.focus();
-  nameBox.innerHTML="";
 
-  errName.innerHTML=("Try again");
-  return false;
-}
 
-name = nameBox.value.toString();
-errName.innerHTML=("");
-nameInput.innerHTML=name + " is your name.";
+  function loadDatabase(){
+    var ref = database.ref("namemess");
+    ref.once("value", gotData, errData);
+
+
 
 }
 
-function gotData(data) {
+function sendToFirebase(){
+  var data = {
+    name: name,
+    message: message}
 
-  var messages = selectAll('.messText');
-  for (var i = 0; i < messages.length; i++) {
-    messages[i].remove();
+
+    var ref = database.ref('namemess');
+    var result = ref.push(data);
+    console.log(result.key);
   }
-
-  //console.log(data.val());
-  var namess = data.val();
-  var keys = Object.keys(namess);
-  //console.log(keys);
-  for (var i = 0; i < keys.length; i++) {
-    var k = keys[i];
-    var na = nemess[k].na;
-    var me = nemess[k].me;
-    //console.log(initials, score);
-    var li = createElement('li', na + ': ' + me);
-    li.class('messText');
-    li.parent('messArea');
-  }
-}
 
 function errData(err) {
   console.log('Error!');
   console.log(err);
 }
+function gotData(data) {
+var messageArea = document.getElementById("section");
+
+  //
+  // console.log(data.val());
+
+  var namemessage = data.val();
+  var keys = Object.keys(namemessage);
 
 
+   console.log(keys);
+   for (var i = 0; i < keys.length; i++) {
+     var k = keys[i];
+     var name = namemessage[k].name;
+     var message = namemessage[k].message;
+     //console.log(initials, score);
+    messageArea.innerHTML += "<br>" + name + " said " + message;
+   }
+
+ }
+
+
+
+
+function enterName(){
+  var nameBox = document.getElementById("namebox");
+  var errName = document.getElementById("errNa");
+  var nameInput = document.getElementById("nameInput");
+  if (nameBox.value=="")
+  {
+    nameBox.focus();
+    nameBox.innerHTML="";
+    errName.innerHTML=("Try again");
+    return false;
+  }
+  name = nameBox.value.toString();
+  errName.innerHTML=("");
+  nameInput.innerHTML=name + " is your name.";
+}
 
 function sendMessage(){
   var messageBox = document.getElementById("messageBox");
@@ -72,36 +88,26 @@ function sendMessage(){
   var errName = document.getElementById("errNa");
   var errMessage = document.getElementById("errMe");
 
-  if (messageBox.value=="" && nameBox.value=="")
+  if (messageBox.value=="")
   {
     messageBox.focus();
-    nameBox.focus();
-    nameBox.innerHTML="";
-    messageBox.innerHTML="";
     errMessage.innerHTML="Try again";
-    errName.innerHTML=("Try again");
     return false;
   }
   if (messageBox.value=="")
   {
     messageBox.focus();
-    messageBox.innerHTML="";
     errMessage.innerHTML="Try again";
     return false;
   }
 
   message = messageBox.value.toString();
-  errMessage.innerHTML="";
+  messageArea.innerHTML += "<br>" + name + " said " + message;
 
-  // messageArea.innerHTML += "<br>" + name + " said " + message;
+sendToFirebase();
 
-  var data = {
-    name: name,
-    message: message}
-
-
-  var ref = database.ref('namemess');
-var result = ref.push(data);
+errMessage.innerHTML="";
 messageBox.value="";
+
 
 }
